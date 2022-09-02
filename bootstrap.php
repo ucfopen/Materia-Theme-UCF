bd80d374d3b4dcd79d9287f0afb9a4269a022ec9
Corey Peterson <corey.peterson@ucf.edu>
2022-09-02 15:12:53 -0400

    Adding scaffolding for js components

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,6 @@ Autoloader::add_classes([
 	'UcfTheme\\UcfTheme'                    => __DIR__.'/classes/ucftheme.php'
 ]);
 
-\Event::register('test_before_widget_login', 'UcfTheme\\UcfTheme::widget_login_redirect');
+\Event::register('before_widget_login', 'UcfTheme\\UcfTheme::widget_login_redirect');
 
 trace('yo check me out');
