90796b29ee35b1440378c3cfaf18ad1b04f67a6a
Corey Peterson <corey.peterson@ucf.edu>
2022-09-02 13:38:03 -0400

    Reworked ucftheme class again with a new method

--- classes/ucftheme.php
+++ classes/ucftheme.php
@@ -4,16 +4,11 @@ namespace UcfTheme;
 
 class UcfTheme
 {
-	public static function widget_redirect()
+	public static function widget_login_redirect()
 	{
-		trace('WIDGET_REDIRECT CLASS CALLED FROM UCFTHEME');
-
-		$this->theme->set_template('layouts/react');
-		$this->theme->get_template()
-			->set('title', 'Login')
-			->set('page_type', 'login');
-
-		Css::push_group(['login']);
-		Js::push_group(['react', 'saml-login']);
+		return [
+			'css' => 'login',
+			'js' => 'saml-login'
+		];
 	}
 }
