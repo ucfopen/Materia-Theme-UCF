7f4d4206dfef27f9480cee16f71e40eb3581d7a5
Corey Peterson <corey.peterson@ucf.edu>
2022-09-21 15:18:14 -0400

    Added lang config to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,7 @@ Autoloader::add_classes([
 	'UcfTheme\\UcfTheme'                    => __DIR__.'/classes/ucftheme.php'
 ]);
 
+\Config::set('materia.lang_path.support', PKGPATH.'ucftheme');
+
 // \Event::register('before_widget_login', 'UcfTheme\\UcfTheme::widget_login_redirect');
 \Event::register('before_help_page', 'UcfTheme\\UcfTheme::help_page_redirect');
